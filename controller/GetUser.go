b61e5edbeb420fc1c1d9010ed8471ce70af9debf
tran ha thu <hathu2516@gmail.com>
2023-02-09 23:16:23 +0800

    Now able to sign out and retrieve user info 	modified:   .env 	new file:   controller/GetUser.go 	new file:   controller/SignOut.go 	modified:   controller/SignUp.go 	modified:   routes/routes.go

---
+++ controller/GetUser.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"github.com/oeggy03/h4g-backend/connect"
+	"github.com/oeggy03/h4g-backend/models"
+	"github.com/oeggy03/h4g-backend/util"
+)
+
+func GetUser(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(util.SecretKey), nil
+	})
+
+	//error handling
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+
+	//Access issuer through claims.Issuer
+	claims := token.Claims.(*jwt.StandardClaims)
+
+	var user models.User
+
+	//Find that user
+	connect.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	c.Status(200)
+	return c.JSON(user)
+}
